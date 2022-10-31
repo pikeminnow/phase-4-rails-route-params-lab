4121ff3d392dd97db536ee2dc3fcf198c3f60518
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-31 18:30:05 -0400

    commit tree e054f2468a6dd39678ac71faec4bd9571bf12803

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,28 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.by_name(params[:name])
+              else    
+                  Student.all
+              end
     render json: students
+  end
+
+def students
+
+  student = Student.find(params[:id])
+  render json: student
+end
+
+  def grades
+    students = Student.all.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.all.order(grade: :desc).first
+    render json: student
   end
 
 end
